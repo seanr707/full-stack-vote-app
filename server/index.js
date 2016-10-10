b06a1ac6d706571fc71a602a227e64be921839c5
seanr707 <seanr.git@gmail.com>
2016-10-09 22:22:16 -0400

    Format server to babel and added scripts

--- server/index.js
+++ server/index.js
@@ -1,18 +1,19 @@
-const express = require('express');
-const session = require('express-session');
-const mongoose = require('mongoose');
-const passport = require('passport');
+import express from 'express';
+import session from 'express-session';
+import mongoose from 'mongoose';
+import passport from 'passport';
 
-const routes = require('./server/routes');
-const models = require('./server/models');
-const oauth = require('./server/oauth');
+import routes from './routes';
+import models from './models';
+import oauth from './oauth';
+
+import { COOKIE_KEY } from './keys';
 
 const app = express();
 
 // Constants
 const DBNAME = 'fcc-voting-app';
-const VIEWDIR = '/public';
-const { COOKIE_KEY } = require('./server/keys');
+const VIEWDIR = process.cwd() + '/public';
 
 app.set('port', (process.env.PORT || 5050));
 
@@ -41,10 +42,10 @@ db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', () => {
   const modelsObj = models();
   oauth(modelsObj);
-  routes(app, modelsObj, process.cwd() + VIEWDIR);
+  routes(app, modelsObj, VIEWDIR);
 });
 
-app.use('/public', express.static(process.cwd() + VIEWDIR));
+app.use('/public', express.static(VIEWDIR));
 
 app.listen(app.get('port'), function () {
   console.log('Node.js listening on port ' + app.get('port') + '...');
