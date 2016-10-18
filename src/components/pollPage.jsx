d8a0f2c429e465c3b724ebd66d63c567db6a5c74
seanr707 <seanr.git@gmail.com>
2016-10-17 23:35:45 -0400

    Vote system now input/radio but styled to be button-esque

--- src/components/pollPage.jsx
+++ src/components/pollPage.jsx
@@ -18,31 +18,6 @@ const Poll = ({ polls, user, dispatch, params }) => {
   const thunkBind = bindActionCreators(thunkActions, dispatch);
   const poll = polls.find(poll => poll._id === params.pollId);
 
-  const tempPoll = {
-    update: {
-      title: 'Edited poll here',
-      desc: 'I was edited',
-      author: {
-        _id: null,
-        name: 'n/a'
-      },
-      options: [
-        {
-          title: 'Yeah',
-          votes: 0
-        },
-        {
-          title: 'Not here',
-          votes: 0
-        },
-        {
-          title: Math.floor(Math.random() * 5).toString(),
-          votes: 0
-        }
-      ]
-    }
-  };
-
   const editButtons = (
     <div className="button-container">
       <button onClick={() => thunkBind.editPoll(poll._id, tempPoll)}>Edit</button>
@@ -50,21 +25,49 @@ const Poll = ({ polls, user, dispatch, params }) => {
     </div>
   );
 
+  const onSubmit = e => {
+    e.preventDefault();
+    const voteId = Array.from(e.target).find(radio => radio.checked).value;
+    thunkBind.votePoll(poll._id, voteId);
+  };
+
+  const options = () => {
+    let selectedVoteId;
+
+    if (user && user.pollsVoted.find(item => item.pollId === poll._id)) {
+      const selectedPoll = user.pollsVoted.find(item => item.pollId === poll._id);
+      selectedVoteId = selectedPoll.optionId;
+    }
+    return (
+      <form className="options" onSubmit={onSubmit}>
+        {poll.options.map((option, i) => {
+          return (
+            <div className="option-container" key={i}>
+              <input
+                id={`option-${i}`}
+                type="radio"
+                name="option"
+                value={option._id}
+                defaultChecked={selectedVoteId === option._id}
+              />
+              <label htmlFor={`option-${i}`} className="option">
+                {option.title}
+              </label>
+            </div>
+          );
+        })}
+        <button type="submit" className="btn btn-default">Vote</button>
+      </form>
+    );
+  };
+
   return (
     <div className="container">
       <div className="page-main row">
         <div className="pollInfo col-8">
           <h3>{poll.title}</h3>
           <p dangerouslySetInnerHTML={markupPoll(poll.desc)} />
-          <ul>
-            {poll.options.map((option, i) => {
-              return (
-                <li key={i} onClick={() => thunkBind.votePoll(poll._id, option._id)}>
-                  {option.title}: {option.votes}
-                </li>
-              );
-            })}
-          </ul>
+          {options()}
         </div>
         <div className="graph col-4">
           <Graph votes={poll.options} />
