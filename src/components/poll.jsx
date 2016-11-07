a83daa2dede178f0c23f37777e89e0ff1d7b2223
seanr707 <seanr.git@gmail.com>
2016-11-07 10:06:51 -0500

    Added verified tag to posts

--- src/components/poll.jsx
+++ src/components/poll.jsx
@@ -21,6 +21,7 @@ const Poll = ({ poll, dispatch }) => {
     <div className="poll-tile row">
       <Link to={`/page/poll/${poll._id}`}>
         <h3 title={`"${poll.desc.substr(0, 20)}..."`} className="col-8">
+          { poll.authRequired ? <span className="tag">Verified</span> : null }
           {poll.title}
         </h3>
         <div className="poll-info-container col-4">
